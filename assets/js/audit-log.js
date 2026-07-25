f4987bd9d278e58a5650f0ceb31778f39ab87673
Daniel666674 <daniel_iel@hotmail.es>
2026-07-25 18:51:16 -0500

    [ivan.escena@gmail.com] Log activity in audit-log.js

--- assets/js/audit-log.js
+++ assets/js/audit-log.js
@@ -50,5 +50,15 @@ window.ESCENA_AUDIT = [
    "manubrio-cromoly-negro-03"
   ],
   "deleted": []
+ },
+ {
+  "ts": "2026-07-25T23:51:15.530Z",
+  "actor": "ivan.escena@gmail.com",
+  "summary": "1 producto editado",
+  "created": [],
+  "edited": [
+   "manubrio-cromoly-negro-01"
+  ],
+  "deleted": []
  }
 ];
