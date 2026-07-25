a42490e5a8bff416dfa1781fb3c4adae9050f6b6
Daniel666674 <daniel_iel@hotmail.es>
2026-07-25 17:33:50 -0500

    [ivan.escena@gmail.com] Log activity in audit-log.js

---
+++ assets/js/audit-log.js
@@ -0,0 +1,12 @@
+window.ESCENA_AUDIT = [
+ {
+  "ts": "2026-07-25T22:33:50.036Z",
+  "actor": "ivan.escena@gmail.com",
+  "summary": "1 producto editado, 2 fotos subidas",
+  "created": [],
+  "edited": [
+   "timon-cult-heavens-gate-4p-cromado"
+  ],
+  "deleted": []
+ }
+];
