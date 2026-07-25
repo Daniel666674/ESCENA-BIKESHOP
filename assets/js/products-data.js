77df3e900cb9e3cf73b49b0216b215a87e170446
Daniel666674 <daniel_iel@hotmail.es>
2026-07-24 20:19:13 -0500

    Update catalog via admin panel

--- assets/js/products-data.js
+++ assets/js/products-data.js
@@ -32,7 +32,7 @@ window.ESCENA_PRODUCTS = [
    }
   ],
   "slug": "timon-gw-destructor-cromado",
-  "img": "assets/img/products/timon-gw-destructor-cromado.jpg?v=1784942152231"
+  "img": "assets/img/products/timon-gw-destructor-cromado.jpg?v=1784942323033"
  },
  {
   "n": "Guantes Gw Largos Full Reflective",
