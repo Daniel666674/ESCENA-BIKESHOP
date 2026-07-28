4501ab7e2e7505a1f1bb0c744accb6fb92978104
Daniel666674 <daniel_iel@hotmail.es>
2026-07-28 12:43:11 -0500

    [tiaghinoacosta@gmail.com] Log activity in audit-log.js

--- assets/js/audit-log.js
+++ assets/js/audit-log.js
@@ -165,5 +165,15 @@ window.ESCENA_AUDIT = [
   ],
   "edited": [],
   "deleted": []
+ },
+ {
+  "ts": "2026-07-28T17:43:10.867Z",
+  "actor": "tiaghinoacosta@gmail.com",
+  "summary": "1 producto nuevo, 1 foto subida",
+  "created": [
+   "marco-profit-culver-gris-cromolio"
+  ],
+  "edited": [],
+  "deleted": []
  }
 ];
