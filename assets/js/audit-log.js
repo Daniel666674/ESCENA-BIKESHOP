ab9458411738b0205a7863bdc016809dd9dd9007
Daniel666674 <daniel_iel@hotmail.es>
2026-07-29 17:31:43 -0500

    [ivan.escena@gmail.com] Log activity in audit-log.js

--- assets/js/audit-log.js
+++ assets/js/audit-log.js
@@ -695,5 +695,15 @@ window.ESCENA_AUDIT = [
    "tenedor-gw-destructor-tornasol"
   ],
   "deleted": []
+ },
+ {
+  "ts": "2026-07-29T22:31:39.982Z",
+  "actor": "ivan.escena@gmail.com",
+  "summary": "1 producto editado, 1 foto subida",
+  "created": [],
+  "edited": [
+   "horquilla-cromoly-negra-01"
+  ],
+  "deleted": []
  }
 ];
