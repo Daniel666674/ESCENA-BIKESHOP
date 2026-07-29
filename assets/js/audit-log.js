12fa925ddc42818d5c41523de8bcf1d6622ffd5b
Daniel666674 <daniel_iel@hotmail.es>
2026-07-29 14:49:43 -0500

    [ivan.escena@gmail.com] Log activity in audit-log.js

--- assets/js/audit-log.js
+++ assets/js/audit-log.js
@@ -360,5 +360,15 @@ window.ESCENA_AUDIT = [
    "timon-fiend-reynolds"
   ],
   "deleted": []
+ },
+ {
+  "ts": "2026-07-29T19:49:40.812Z",
+  "actor": "ivan.escena@gmail.com",
+  "summary": "1 producto editado, 1 foto subida",
+  "created": [],
+  "edited": [
+   "timon-fiend-reynolds"
+  ],
+  "deleted": []
  }
 ];
