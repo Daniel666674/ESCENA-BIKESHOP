5150327f3adea68247fe8e9d12bef82994037d52
Daniel666674 <daniel_iel@hotmail.es>
2026-07-29 14:23:03 -0500

    [ivan.escena@gmail.com] Update catalog via admin panel

--- assets/js/products-data.js
+++ assets/js/products-data.js
@@ -117,8 +117,8 @@ window.ESCENA_PRODUCTS = [
   "slug": "timon-gw-gorila-4pc-negro",
   "imgFit": "cover",
   "imgs": [
-   "assets/img/products/timon-gw-gorila-4pc-negro.jpg?v=1784944288272",
-   "assets/img/products/timon-gw-gorila-4pc-negro-2.jpg?v=1785352578738"
+   "assets/img/products/timon-gw-gorila-4pc-negro-2.jpg?v=1785352578738",
+   "assets/img/products/timon-gw-gorila-4pc-negro.jpg?v=1784944288272"
   ],
   "colors": [
    {
@@ -133,14 +133,14 @@ window.ESCENA_PRODUCTS = [
    }
   ],
   "imgColorMap": {
-   "Negro": [
+   "Morado": [
     0
    ],
-   "Morado": [
+   "Negro": [
     1
    ]
   },
-  "img": "assets/img/products/timon-gw-gorila-4pc-negro.jpg?v=1784944288272"
+  "img": "assets/img/products/timon-gw-gorila-4pc-negro-2.jpg?v=1785352578738"
  },
  {
   "n": "Timon Gw Destructor",
