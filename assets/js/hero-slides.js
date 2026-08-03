be3a2ba8bdba7d2344c3845557a760d84a3a8567
Daniel666674 <daniel_iel@hotmail.es>
2026-08-03 18:35:45 -0500

    [daniel.f.acosta96@gmail.com] Update hero slides via admin panel

--- assets/js/hero-slides.js
+++ assets/js/hero-slides.js
@@ -1,7 +1,18 @@
 window.ESCENA_HERO_SLIDES = [
- { "id": "seed-1", "img": "assets/img/products/federal-horquilla-cromada.jpg" },
- { "id": "seed-2", "img": "assets/img/products/eclat-trippin-rin-negro.jpg" },
- { "id": "seed-3", "img": "assets/img/products/cult-bielas-negro.jpg" },
- { "id": "seed-4", "img": "assets/img/products/bsd-stem-negro.jpg" },
- { "id": "seed-5", "img": "assets/img/products/odyssey-buje-cassette-negro.jpg" }
+ {
+  "id": "seed-1",
+  "img": "assets/img/products/federal-horquilla-cromada.jpg"
+ },
+ {
+  "id": "seed-2",
+  "img": "assets/img/products/eclat-trippin-rin-negro.jpg"
+ },
+ {
+  "id": "seed-3",
+  "img": "assets/img/products/cult-bielas-negro.jpg"
+ },
+ {
+  "id": "seed-4",
+  "img": "assets/img/products/bsd-stem-negro.jpg"
+ }
 ];
