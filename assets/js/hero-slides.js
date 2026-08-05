0a74705944dd37715578b62c1ce785ee7a6c7e38
Daniel666674 <daniel_iel@hotmail.es>
2026-08-04 21:48:44 -0500

    [daniel.f.acosta96@gmail.com] Update hero slides via admin panel

--- assets/js/hero-slides.js
+++ assets/js/hero-slides.js
@@ -1,6 +1,10 @@
 window.ESCENA_HERO_SLIDES = [
  {
   "id": "seed-1",
-  "img": "assets/img/products/federal-horquilla-cromada.jpg"
+  "img": "assets/img/products/federal-horquilla-cromada.jpg",
+  "video": {
+   "v": 1785898124272,
+   "ext": "mp4"
+  }
  }
 ];
