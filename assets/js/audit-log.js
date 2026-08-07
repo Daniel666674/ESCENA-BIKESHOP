161292512de453195ee93b5d8600b5dad6b99ba6
Daniel666674 <daniel_iel@hotmail.es>
2026-08-07 12:39:22 -0500

    [ivan.escena@gmail.com] Log activity in audit-log.js

--- assets/js/audit-log.js
+++ assets/js/audit-log.js
@@ -1333,5 +1333,15 @@ window.ESCENA_AUDIT = [
    "kit-eje-espaciadores-plata"
   ],
   "deleted": []
+ },
+ {
+  "ts": "2026-08-07T17:39:21.754Z",
+  "actor": "ivan.escena@gmail.com",
+  "summary": "1 producto editado, 1 foto subida",
+  "created": [],
+  "edited": [
+   "kit-eje-espaciadores-plata"
+  ],
+  "deleted": []
  }
 ];
