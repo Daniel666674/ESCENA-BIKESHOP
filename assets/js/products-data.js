248071d03c4fb85a7b6f62ae994a88c4e970e2f9
Daniel666674 <daniel_iel@hotmail.es>
2026-08-07 16:53:18 -0500

    [ivan.escena@gmail.com] Update catalog via admin panel

--- assets/js/products-data.js
+++ assets/js/products-data.js
@@ -3736,7 +3736,7 @@ window.ESCENA_PRODUCTS = [
   "sku": "CULT-PUNOS-RASTA",
   "units": 3,
   "order": 10,
-  "img": "assets/img/products/cult-punos-rasta.jpg?v=1786139522117"
+  "img": "assets/img/products/cult-punos-rasta.jpg?v=1786139557532"
  },
  {
   "slug": "odyssey-punos-negro-gris",
