073679fe95a1ffb5b7ad57c62f092c51902b2cd7
Daniel666674 <daniel_iel@hotmail.es>
2026-08-22 10:09:32 -0500

    [camilor9507@gmail.com] Log activity in audit-log.js

--- assets/js/audit-log.js
+++ assets/js/audit-log.js
@@ -3190,5 +3190,15 @@ window.ESCENA_AUDIT = [
    "marco-federal-command-negro-20-5-x-cola-12-5"
   ],
   "deleted": []
+ },
+ {
+  "ts": "2026-08-22T15:09:29.067Z",
+  "actor": "camilor9507@gmail.com",
+  "summary": "1 producto editado, 1 foto subida",
+  "created": [],
+  "edited": [
+   "marco-federal-chiller-20-75-cola-12-7"
+  ],
+  "deleted": []
  }
 ];
