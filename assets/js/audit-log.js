275b5bec0767980fe6bc0e976c9823a03ba2626d
Daniel666674 <daniel_iel@hotmail.es>
2026-08-25 15:16:24 -0500

    [ivan.escena@gmail.com] Log activity in audit-log.js

--- assets/js/audit-log.js
+++ assets/js/audit-log.js
@@ -3693,5 +3693,15 @@ window.ESCENA_AUDIT = [
   ],
   "edited": [],
   "deleted": []
+ },
+ {
+  "ts": "2026-08-25T20:16:23.987Z",
+  "actor": "ivan.escena@gmail.com",
+  "summary": "1 producto nuevo, 2 fotos subidas",
+  "created": [
+   "manzana-delantera-ontrail-nemesis"
+  ],
+  "edited": [],
+  "deleted": []
  }
 ];
