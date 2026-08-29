263547f61062027f7aac0b5f56d2038923070828
Daniel666674 <daniel_iel@hotmail.es>
2026-08-29 15:58:12 -0500

    [tiaghinoacosta@gmail.com] Log activity in audit-log.js

--- assets/js/audit-log.js
+++ assets/js/audit-log.js
@@ -4389,5 +4389,15 @@ window.ESCENA_AUDIT = [
   ],
   "edited": [],
   "deleted": []
+ },
+ {
+  "ts": "2026-08-29T20:58:14.241Z",
+  "actor": "tiaghinoacosta@gmail.com",
+  "summary": "1 producto nuevo, 2 fotos subidas",
+  "created": [
+   "kit-ruedas-minilogo-x4"
+  ],
+  "edited": [],
+  "deleted": []
  }
 ];
