6b9afe73f9301a6a5391de4d84f511f2f1c575c6
Daniel666674 <daniel_iel@hotmail.es>
2026-08-29 15:24:28 -0500

    [tiaghinoacosta@gmail.com] Log activity in audit-log.js

--- assets/js/audit-log.js
+++ assets/js/audit-log.js
@@ -4349,5 +4349,15 @@ window.ESCENA_AUDIT = [
   "deleted": [
    "trucks-koston-2"
   ]
+ },
+ {
+  "ts": "2026-08-29T20:24:30.442Z",
+  "actor": "tiaghinoacosta@gmail.com",
+  "summary": "1 producto nuevo, 1 foto subida",
+  "created": [
+   "pivots-monk"
+  ],
+  "edited": [],
+  "deleted": []
  }
 ];
