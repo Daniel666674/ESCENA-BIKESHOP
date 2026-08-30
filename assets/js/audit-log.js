73c4c7472c67a4047f57445de76e8de3c7bebd94
Daniel666674 <daniel_iel@hotmail.es>
2026-08-30 11:43:31 -0500

    [camilor9507@gmail.com] Log activity in audit-log.js

--- assets/js/audit-log.js
+++ assets/js/audit-log.js
@@ -4450,5 +4450,15 @@ window.ESCENA_AUDIT = [
   ],
   "edited": [],
   "deleted": []
+ },
+ {
+  "ts": "2026-08-30T16:43:30.600Z",
+  "actor": "camilor9507@gmail.com",
+  "summary": "1 producto nuevo, 3 fotos subidas",
+  "created": [
+   "cadena-tall-order-cromada-510"
+  ],
+  "edited": [],
+  "deleted": []
  }
 ];
