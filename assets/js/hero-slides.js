1f66172900085b5e074eb16b4f399dcace585958
Daniel666674 <daniel_iel@hotmail.es>
2026-09-06 07:20:25 -0500

    [daniel.f.acosta96@gmail.com] Update hero slides via admin panel

--- assets/js/hero-slides.js
+++ assets/js/hero-slides.js
@@ -5,5 +5,9 @@ window.ESCENA_HERO_SLIDES = [
    "v": 1785904000000,
    "ext": "mp4"
   }
+ },
+ {
+  "id": "hmtps56qphhbo",
+  "img": "assets/img/hero/slide-hmtps56qphhbo.jpg?v=1788697225307"
  }
 ];
