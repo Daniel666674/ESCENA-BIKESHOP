5f3e788598ca18a929c0a909a0586c48b6f825b6
Daniel666674 <daniel_iel@hotmail.es>
2026-09-25 13:01:03 -0500

    [ivan.escena@gmail.com] Log activity in audit-log.js

--- assets/js/audit-log.js
+++ assets/js/audit-log.js
@@ -6720,5 +6720,15 @@ window.ESCENA_AUDIT = [
    "llanta-innova-pro-negra-2-40"
   ],
   "deleted": []
+ },
+ {
+  "ts": "2026-09-25T18:00:59.946Z",
+  "actor": "ivan.escena@gmail.com",
+  "summary": "1 producto editado, 1 foto subida",
+  "created": [],
+  "edited": [
+   "pedales-merritt-p1-aqua"
+  ],
+  "deleted": []
  }
 ];
